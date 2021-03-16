0223601616aa72120f7e51246bc3de1a1027b4a3
Ania Chepaikina <belka@belka.home>
2021-03-16 14:25:43 +0100

    Update

--- download_corpus.sh
+++ download_corpus.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-# ------------------------------------------
+# I. --------------------------------------------
+
 INPUT=$1 # path to csv file
 OLDIFS=$IFS
 IFS=','
-
-
 i=1 # start counting rows
 
-# open file
-[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 
-# loop through rows
-while read id pdf html html_abby stades # specify columns from the csv file
+
+# II. -------------------------------------------
+
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; } # open csv file
+
+while read id pdf html html_abby stades # # loop through rows and columns
 do
 	echo i: $i # current number of the row
 	echo "Name : $id" # column that contains specific ids of the bulletins
@@ -21,15 +22,16 @@ do
 	((i++)) # increment the count
 done < $INPUT
 
-# show last row
+
 echo "Last row : "
 echo $id
 echo $html
-wget -p -k $html # download html
-
-# move all html files to another location
-mkdir ./resources/html_corpus
-find . -name "*.html" -exec mv "{}" ./resources/html_corpus \;
-rm -rf ontology.inrae.fr
+wget -p -k $html # download last html
 
 IFS=$OLDIFS
+
+
+# III. ------------------------------------------
+mkdir ./resources/html_corpus # move all html files to another location
+find . -name "*.html" -exec mv "{}" ./resources/html_corpus \;
+rm -rf ontology.inrae.fr
