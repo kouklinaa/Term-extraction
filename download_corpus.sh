caa9e54fc700a6b279d98cd572dbbdc2b773b0f0
Ania Chepaikina <belka@belka.home>
2021-03-16 14:28:51 +0100

    Update

--- download_corpus.sh
+++ download_corpus.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# I. --------------------------------------------
+
 
 INPUT=$1 # path to csv file
 OLDIFS=$IFS
-IFS=','
-i=1 # start counting rows
+IFS=',' # separator
+i=1 # intitialize count
 
 
 
-# II. -------------------------------------------
+# -------------------DOWNLOAD------------------------
 
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; } # open csv file
 
-while read id pdf html html_abby stades # # loop through rows and columns
+while read id pdf html html_abby stades # loop through rows and columns
 do
 	echo i: $i # current number of the row
 	echo "Name : $id" # column that contains specific ids of the bulletins
@@ -31,7 +31,7 @@ wget -p -k $html # download last html
 IFS=$OLDIFS
 
 
-# III. ------------------------------------------
+# -------------------SAVE--------------------------
 mkdir ./resources/html_corpus # move all html files to another location
 find . -name "*.html" -exec mv "{}" ./resources/html_corpus \;
 rm -rf ontology.inrae.fr
